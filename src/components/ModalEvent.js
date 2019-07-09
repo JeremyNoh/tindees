23a02430da534686ea3c250add16a8409b1cfd72
Jeremy Noh <jeremy@wertkt.com>
2019-07-09 22:44:29 +0200

    add description of the event

--- src/components/ModalEvent.js
+++ src/components/ModalEvent.js
@@ -7,13 +7,15 @@ import {
   View,
   Image,
   TouchableOpacity,
-  Dimensions,
-  Text
+  Dimensions
 } from "react-native";
 import Title from "./Title";
-import { Button, Card } from "react-native-elements";
+import { Button, Card, Text } from "react-native-elements";
 import { joinEvent, deleteEvent } from "../../api/event";
 import { BUTTON_COLOR_ONE, BACKGROUND_BODY } from "../../utils/colors";
+import DatePicker from "react-native-datepicker";
+import Container from "./Container";
+
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
 
@@ -59,8 +61,6 @@ export const ModalEvent = ({
       });
   };
 
-  console.log(event);
-
   return (
     <Overlay
       isVisible={true}
@@ -71,65 +71,159 @@ export const ModalEvent = ({
       width={width - 50}
       animationType="slide"
     >
-      <View>
-        <TouchableOpacity
-          onPress={() => {
-            isClose(false);
-          }}
-        >
-          <Image
-            source={require("../../assets/cross.png")}
-            style={{ width: 35, height: 35 }}
-          />
-        </TouchableOpacity>
-        <Card title={event.name}>
-          <Text>{event.description}</Text>
-        </Card>
-
+      <Container>
         <View>
-          <View>
-            <Text>date de Debut</Text>
-            <Text>date Fin</Text>
-          </View>
+          <TouchableOpacity
+            onPress={() => {
+              isClose(false);
+            }}
+          >
+            <Image
+              source={require("../../assets/cross.png")}
+              style={{ width: 35, height: 35 }}
+            />
+          </TouchableOpacity>
+          <Card title={event.name} titleStyle={styles.title}>
+            <View style={{ alignItems: "center" }}>
+              <View style={styles.alignElement}>
+                <Text style={styles.title}>Catégorie : </Text>
+                <Text>{event.category}</Text>
+              </View>
+              <View style={styles.alignElement}>
+                <Text style={styles.title}>Description : </Text>
+                <Text>{event.description}</Text>
+              </View>
+              <View style={{ alignItems: "center", marginTop: 20 }}>
+                <Text style={styles.title}>Date de L'event : </Text>
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    marginTop: 5
+                  }}
+                >
+                  <View style={{ marginRight: 20 }}>
+                    <DatePicker
+                      date={event.startDate}
+                      mode="datetime"
+                      placeholder="Debut de l'event"
+                      minDate={new Date()}
+                      confirmBtnText="Confirm"
+                      cancelBtnText="Cancel"
+                      is24Hour={true}
+                      disabled
+                      style={{
+                        width: 140,
+                        borderWidth: 1,
+                        borderColor: "#EDF0F2",
+                        borderRadius: 6
+                      }}
+                      customStyles={{
+                        dateIcon: {
+                          position: "absolute",
+                          left: 0,
+                          top: 4,
+                          marginLeft: 0,
+                          height: 0,
+                          width: 0
+                        },
+                        dateInput: {
+                          marginLeft: 36
+                        },
+                        dateInput: {
+                          borderWidth: 0,
+                          borderBottomWidth: 0.5,
+                          borderColor: BUTTON_COLOR_ONE
+                        }
+                      }}
+                    />
+                  </View>
+
+                  <View>
+                    <DatePicker
+                      date={event.endDate}
+                      mode="datetime"
+                      placeholder="Fin de l'event"
+                      confirmBtnText="Confirm"
+                      cancelBtnText="Cancel"
+                      is24Hour={true}
+                      disabled
+                      style={{
+                        width: 140,
+                        borderWidth: 1,
+                        borderColor: "#EDF0F2",
+                        borderRadius: 6
+                      }}
+                      customStyles={{
+                        dateIcon: {
+                          position: "absolute",
+                          left: 0,
+                          top: 4,
+                          marginLeft: 0,
+                          height: 0,
+                          width: 0
+                        },
+                        dateInput: {
+                          marginLeft: 36
+                        },
+                        dateInput: {
+                          borderWidth: 0,
+                          borderBottomWidth: 0.5,
+                          borderColor: BUTTON_COLOR_ONE
+                        }
+                      }}
+                    />
+                  </View>
+                </View>
+              </View>
+              <View style={[styles.alignElement]}>
+                <Text style={styles.title}>Adresse : </Text>
+                <Text>
+                  {event.address} {event.zip_code}
+                </Text>
+              </View>
+            </View>
+          </Card>
+
+          {isRegistered ? (
+            <Button
+              onPress={() => {
+                _deleteEvent();
+              }}
+              buttonStyle={[
+                styles.Button,
+                {
+                  height: 50,
+                  backgroundColor: BACKGROUND_BODY,
+                  borderWidth: 1,
+                  borderColor: BUTTON_COLOR_ONE,
+                  borderRadius: 5
+                }
+              ]}
+              title="Se desinscrire"
+              titleStyle={{ color: "black" }}
+            />
+          ) : (
+            <Button
+              onPress={() => {
+                _joinEvent();
+              }}
+              buttonStyle={[
+                styles.Button,
+                {
+                  height: 50,
+                  backgroundColor: BACKGROUND_BODY,
+                  borderWidth: 1,
+                  borderColor: BUTTON_COLOR_ONE,
+                  borderRadius: 5
+                }
+              ]}
+              title="Je Participe"
+              titleStyle={{ color: "black" }}
+            />
+          )}
         </View>
-        {isRegistered ? (
-          <Button
-            onPress={() => {
-              _deleteEvent();
-            }}
-            buttonStyle={[
-              styles.Button,
-              {
-                height: 50,
-                backgroundColor: BACKGROUND_BODY,
-                borderWidth: 1,
-                borderColor: BUTTON_COLOR_ONE,
-                borderRadius: 5
-              }
-            ]}
-            title="Se desinscrire"
-            titleStyle={{ color: "black" }}
-          />
-        ) : (
-          <Button
-            onPress={() => {
-              _joinEvent();
-            }}
-            buttonStyle={[
-              styles.Button,
-              {
-                height: 50,
-                backgroundColor: BACKGROUND_BODY,
-                borderWidth: 1,
-                borderColor: BUTTON_COLOR_ONE,
-                borderRadius: 5
-              }
-            ]}
-            title="Je Participe"
-            titleStyle={{ color: "black" }}
-          />
-        )}
-      </View>
+      </Container>
     </Overlay>
   );
 };
@@ -143,6 +237,11 @@ const styles = StyleSheet.create({
     borderRadius: 5
   },
   alignElement: {
-    alignItems: "center"
+    alignItems: "center",
+    marginTop: 20
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold"
   }
 });
